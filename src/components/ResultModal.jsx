31a04922b615bd07cbf03e02ac5aab415ff1c12f
JHS96 <jschaper@hotmail.co.za>
2025-05-22 15:09:50 +0200

    Win/lose condition complete

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,10 +4,14 @@ import { forwardRef, useImperativeHandle, useRef } from 'react';
 // which is used here for demonstration purposes, as it works on both current and older React versions.
 // export default function ResultModal({ ref, result, targetTime }) {
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -18,15 +22,17 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialog} className='result-modal'>
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className='result-modal' onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds remaining.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedRemainingTime} seconds remaining.</strong>
       </p>
-      <form method='dialog'>
+      <form method='dialog' onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
